d0153ac93274789fbaf7b8bb69fa5d3ad4eaa2ad
tanya3685 <52120289+tanya3685@users.noreply.github.com>
2019-06-23 13:21:18 +0800

    TEST
    TEST

---
+++ js/roll.js
@@ -0,0 +1,15 @@
+ $("#t1").mouseover(function() {
+     $("#img1").fadeIn();
+     $("#img2").fadeOut();
+     $("#img3").fadeOut();
+ });
+ $("#t2").mouseover(function() {
+     $("#img1").fadeOut();
+     $("#img2").fadeIn();
+     $("#img3").fadeOut();
+ });
+ $("#t3").mouseover(function() {
+     $("#img1").fadeOut();
+     $("#img2").fadeOut();
+     $("#img3").fadeIn();
+ });
